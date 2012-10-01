be9b01b1f1768e51c356bedd7723bf88b5a42e59
= <=>
2012-10-01 13:41:22 -0700

    setting up

--- config.php
+++ config.php
@@ -2,27 +2,27 @@
 
 // DONT FORGET TO EDIT JS CONFIG VARIABLES
 
-$site['name'] = 'Love Ottawa';
+$site['name'] = 'NorthBC';
 
 $http = 'http';
 if(isset($_SERVER['HTTPS']) == 'on') $http .= 's';
 $pageUrl = $http . '://' . $_SERVER['SERVER_NAME'] . '/';
 
-define('BASE_URL', $pageUrl . 'ottawa/www/');
-define('BASE_PATH', '/var/www/html/clients/ottawa/');
+define('BASE_URL', $pageUrl . 'nbc/www/');
+define('BASE_PATH', '/var/www/html/clients/nbc/');
 define('CLASS_PATH', BASE_PATH . 'classes/');
 define('SITE_PATH', BASE_PATH . 'www/');
 define('UPLOAD_PATH', SITE_PATH . 'uploads/');
 
-define('DB_NAME', 'ottawa');
+define('DB_NAME', 'nbc');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'ottawa');
+define('DB_USER', 'nbc');
 define('DB_PASS', 'letmein!');
 
-define('APP_ID', '368337929912122');
-define('APP_SECRET', 'e07c419fc57be521f98409fb3cdbd0f5');
+define('APP_ID', '183664281769605');
+define('APP_SECRET', '82ddaa576fd54f7fdd72c7f487eb9c4b');
 
-define('PAGE_TAB', 'http://www.facebook.com/pages/Telenova-IP-Services/287547201338150?sk=app_368337929912122');
+define('PAGE_TAB', 'http://www.facebook.com/pages/Telenova-IP-Services/287547201338150?sk=app_183664281769605');
 
 require_once(BASE_PATH . 'common.php');
 
