13d91e7e2cee4a82092a1ae6948e235b6960bc7c
Cody Halovich <cody@telenova.ca>
2012-10-03 20:06:56 -0700

    removed uploads dir from git

---
+++ admin/images.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('../config.php');
+
+if(isset($_POST['dropdown']) && !empty($_POST['dropdown'])
+&& $_FILES['file']['name'] != '') {
+
+	$dropdown = $_POST['dropdown'];
+
+	if($dropdown == 'right') {
+		$result = move_uploaded_file($_FILES['file']['tmp_name'], UPLOAD_PATH . 'votecallout.png');
+		if(!$result) $upFail = true;
+	} else if ($dropdown == 'bottom') {
+		$result = move_uploaded_file($_FILES['file']['tmp_name'], UPLOAD_PATH . 'bottombar.png');
+		if(!$result) $upFail = true;
+	}
+
+	if(isset($upFail)) {
+		echo "Image Upload Failed.";
+	} else {
+		echo "Image Upload Successful";
+	}
+
+}
+?>
+
+<form method="POST" action="<?=$_SERVER['PHP_SELF'];?>?p=images" enctype="multipart/form-data" name="image_uploader" id="image_uploader">
+
+	<label for="dropdown">Which Image Would you like to replace?</label>
+	<select name="dropdown">
+		<option value="right">Right Side</option>
+		<option value="bottom">Bottom Bar</option>
+	</select>
+	<input type="file" name="file" id="file"/>
+	<input type="submit" value="Upload and Replace" name="submit" id="submit"/>
+
+</form>
---
+++ www/ajax/vote.php
@@ -0,0 +1,53 @@
+<?php
+
+require('../../config.php');
+$date = new MongoDate();
+$status = 200;
+
+if($_POST['vote'] && $_POST['ip']) {
+
+	// GET CURRENT CAMPAIGN
+	try {
+		$campaign = $db->select('campaign', array('start' => array('$lt'=>$date),'end'=>array('$gt'=>$date)));
+	} catch (Exception $e) {
+		$status = 500;
+		$error[] = $e->getMessage();
+	}
+
+	// PULL CAMPAIGN FROM MONGOCURSOR
+	foreach($campaign as $camp);
+
+	$checkVote = $db->count('votes',array('campaign' => $camp['_id'],'ip'=>(string)$_POST['ip']));
+
+	if($checkVote > 0) {
+		$error[] = 'You are not eligible to vote';
+		$status = 502;
+	}
+
+	// CRITERIA FOR UPDATE
+	$crit = $camp['_id'];
+
+	// CONVERT VOTE TO 0 BASED INT
+	$for = $_POST['vote_for']-1;
+
+	// LOG VOTE IN VOTES COLLECTION
+	if($status == 200) {
+		try {
+			$result = $db->insert('votes',array('ip'=>$_POST['ip'],'campaign'=>$camp['_id'],'vote'=>$for));
+		} catch (Exception $e) {
+			$status = 500;
+			$error[] = $e->getMessage();
+		}
+	}
+
+
+}
+
+// SETUP AND THROW RESPONSE
+$response = array('status' => $status);
+if(isset($error)) 
+	$response['errors'] = $error;
+
+echo json_encode($response);
+
+?>
